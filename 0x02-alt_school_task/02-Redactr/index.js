a4cc439547fec40df3a737c4213de7f5b38bd8d7
Abdulmalik Sanni A <31365779+muftisanni@users.noreply.github.com>
2023-12-02 11:16:46 +0100

    ALX Borrowed Content
    I am not ALX Student. Just learning anything I committed

---
+++ 0x02-alt_school_task/02-Redactr/index.js
@@ -0,0 +1,153 @@
+//the following function gets and seperate word to an array
+const getWords = (str) => {
+    let arr = str.split(" ");
+    return arr;
+  }
+  // the following function will check if user enters the correct inputs 
+  const validateInput = (input1, input2, input3) => {
+    if (input1.value == "" || input2.value == "" || (input3.value).length > 1) {
+      if (input1.value == "") {
+        input1.style = "border: 1px solid red";
+        err.innerHTML = "Please provide the content";
+      } else if (input2.value == "") {
+        input1.style = "border: 1px solid green";
+        input2.style = "border: 1px solid red";
+        err.innerHTML = "Please provide the words to redact";
+      } else if ((input3.value).length > 1) {
+        input2.style = "border: 1px solid green";
+        input3.style = "border: 1px solid red";
+        err.innerHTML = "Please provide only one characher";
+      }
+      return false;
+    }
+    input1.style = "border: 1px solid green";
+    input2.style = "border: 1px solid green";
+    input3.style = "border: 1px solid green";
+    err.innerHTML = "";
+    return true;
+  }
+  
+  
+  function rep(text, replaceWith = "#") {
+    replaceWith = replaceWith == "" ? "#" : replaceWith;
+    let replaceText = "";
+    for (let i = 0; i < text.length; i++) {
+      replaceText += "" + replaceWith + "";
+    }
+    return replaceText;
+  }
+  
+  const replacer = (arr) => {
+    let replaceArr = arr.map(item => {
+      item = rep(item, replacement.value);
+      return item;
+    })
+    return replaceArr;
+  }
+  // Advanced featured 
+  const wordCount = (sepWords) => {
+    let matchCount = 0;
+    sepWords.forEach(word => {
+      let regex = new RegExp(word, "gi");
+      matchCount += (content.value).match(regex).length;
+    });
+    return matchCount;
+  }
+  const charCount = sepWords => {
+    let matchCount = 0;
+    sepWords.forEach(word => {
+      let regex = new RegExp(word, "gi");
+      (content.value).match(regex).forEach(char => {
+        matchCount += char.length;
+      })
+    });
+    return matchCount;
+  }
+  const scannedWords = str => {
+    return getWords(str.value).length;
+  }
+  
+  const checkStat = () => {
+    stats.innerHTML = `
+      <h3>Stats</h3>
+      <hr>
+      <p>Scanned Word(s): <span>${scannedWords(content)} ${scannedWords(content) < 2 ? "word" : "words"}</span></p>
+      <p>Matched Word(s): <span>${wordCount(getWords(words.value))} ${wordCount(getWords(words.value)) < 2 ? "word" : "words"}</span></p>
+      <p>Character Count(s): <span>${charCount(getWords(words.value))} ${charCount(getWords(words.value)) < 2 ? "character" : "characters"}</span></p>
+      <p>Time Completed: <span>${timeTaken.toFixed(2)}ms</span></p>
+    `;
+  }
+  
+  // getting elements with DOM 
+  const content = document.getElementById("content");
+  const words = document.getElementById("words");
+  const redactr = document.getElementById("form");
+  const replacement = document.getElementById("replacement");
+  const preview = document.getElementById("preview");
+  const err = document.getElementById("err");
+  const previewContainer = document.getElementsByClassName("preview-container")[0];
+  const howItWorks = document.getElementsByClassName("mode-of-operation")[0];
+  const stats = document.getElementsByClassName("stat")[0];
+  const backBtn = document.getElementsByClassName("back")[0];
+  const copy = document.getElementsByClassName("fa-clipboard")[0];
+  const socialBtn = document.querySelectorAll('.social-btn');
+  let timeTaken;
+  //this function handles every converstion at an event
+  function redactWords() {
+    let formValidation = validateInput(content, words, replacement);
+    if (formValidation) {
+      let secretWords = getWords(words.value);
+      let redactedArr = replacer(secretWords);
+      let contentValue = content.value;
+  
+      for (let i = 0; i < secretWords.length; i++) {
+        contentValue = contentValue.replaceAll(secretWords[i], redactedArr[i]);
+      }
+  
+      preview.innerHTML = contentValue;
+      previewContainer.style = "display: block;";
+      modeofoperation.style.display = "none";
+      redactr.style.display = "none";
+  
+    }
+  }
+  
+  //submit event 
+  redactr.onsubmit = (e) => {
+    e.preventDefault(); //prevents the default form submission
+    let start = performance.now();
+    redactWords();
+    let end = performance.now();
+    timeTaken = end - start;
+    checkStat();
+  }
+  // back button 
+  backBtn.onclick = () => {
+    previewContainer.style = "display: none;";
+    modeofoperation.style.display = "block";
+    redactr.style.display = "block";
+  }
+  
+  copy.onclick = () => {
+    //NB. doesn't work on iframes, switch to full display
+    navigator.clipboard.writeText(preview.innerText);
+    alert("Copied!");
+  }
+  
+  socialBtn.forEach(btn => {
+    btn.onclick = () => {
+      switch (btn.classList[1]) {
+        case "whatsapp":
+          btn.setAttribute('href', `https://wa.me/?text=${(preview.innerText).toLowerCase()}`)
+          break;
+        case "sms":
+          btn.setAttribute('href', `sms:body=${(preview.innerText).toLowerCase()}`)
+          break;
+        case "mail":
+          btn.setAttribute('href', `mailto:subject=secretmessage&body=${(preview.innerText).toLowerCase()}`)
+          break;
+        default:
+          console.log("error");
+      }
+    }
+  })
---
+++ 0x02-alt_school_task/README.md
@@ -0,0 +1,31 @@
+### 02- Redactr - Scramble some words from a body of text content
+
+Project done during Frontend Software Engineering studies at **ALTSCHOOL AFRICA SCHOOL OF ENGINEERING**. In this project, I used `HTML`, `CSS` and `JavaScript` to build a web app that could help you safely share content on social media by scrambling portions of texts you wouldn't want the world to see/read.
+
+## Overview 👉🏼: [Redactr-App](https://redactr-app.succynice.repl.co/)
+
+*****
+
+## Scenario
+
+[Redactr-App](https://redactr-app.succynice.repl.co/) is to help people safely share content on social media by scrambling portions of the text they don’t want the world to read/see. A user will write/paste some content into a form field and then specify the words they need to be scrambled (using another field). **Note** the user is allowed to specify more than one word (separated by space) to be scrambled.
+
+## Outcome
+After clicking a “Redact Now” button, the app scans through the original text and replaces the specified words to be scrambled with asterisks (i.e ***)
+
+## Advanced features
+
+*User can specify what to use in replacing the words, then the app can do the scrambling with what the user provides, be it asterisks, questions marks, dashes, underscores, or even other words
+* Displays some stats like how many words were scanned, how many matched the words to be scrambled, how many characters were scrambled in total, how long it took (in seconds) to complete the scrambling task e.t.c.
+
+********
+
+## Built With 🛠
+  * HTML5
+  * CSS3
+  * JavaScript
+
+## Contact 🤙
+  - Github: [@sanni](https://github.com/muftisanni)
+  - Twitter: [@ssanni](https://twitter.com/matchket)
+
